b0cd6743d45872a9255c6b4c869e17de6e9ad4f4
Ralph Avalon <amoedo.raphael@gmail.com>
2018-05-12 18:25:16 -0300

    Change position of vote score

--- frontend/src/components/voteScore/index.js
+++ frontend/src/components/voteScore/index.js
@@ -7,7 +7,7 @@ class VoteScore extends Component {
             <small className="pull-right text-muted">
                 <div style={{ "display": "inline-grid" }}>
                     <i className="fa fa-chevron-up fa-fw" onClick={onUpVote}></i>
-                    <span className={"text-center vote-score " + (voteScore == 0 ? 'neutral-vote-score' : voteScore > 0 ? 'positive-vote-score' : 'negative-vote-score')}>{voteScore}</span>
+                    <span className={"text-center vote-score " + (voteScore === 0 ? 'neutral-vote-score' : voteScore > 0 ? 'positive-vote-score' : 'negative-vote-score')}>{voteScore}</span>
                     <i className="fa fa-chevron-down fa-fw" onClick={onDownVote}></i>
                 </div>
                 
