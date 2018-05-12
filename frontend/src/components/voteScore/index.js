8031f6fcaf0e3ea7dc062827124247ba4f1de7be
Ralph Avalon <amoedo.raphael@gmail.com>
2018-05-12 15:55:56 -0300

    Fix voteScore exhibition

--- frontend/src/components/voteScore/index.js
+++ frontend/src/components/voteScore/index.js
@@ -6,12 +6,11 @@ class VoteScore extends Component {
         return (
             <small className="pull-right text-muted">
                 <div style={{ "display": "inline-grid" }}>
-                    <i className="fa fa-chevron-up fa-fw" onClick={onUpVote}>
-                        
-                    </i>
+                    <i className="fa fa-chevron-up fa-fw" onClick={onUpVote}></i>
+                    <span className={"text-center vote-score " + (voteScore == 0 ? 'neutral-vote-score' : voteScore > 0 ? 'positive-vote-score' : 'negative-vote-score')}>{voteScore}</span>
                     <i className="fa fa-chevron-down fa-fw" onClick={onDownVote}></i>
                 </div>
-                <span className={"vote-score " + (voteScore == 0 ? 'neutral-vote-score' : voteScore > 0 ? 'positive-vote-score' : 'negative-vote-score')}>{voteScore}</span>
+                
             </small>
         );
     }
