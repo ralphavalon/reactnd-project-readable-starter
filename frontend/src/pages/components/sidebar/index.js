fbd0547c4f5928c24d47865e6f0bffae53da24aa
Ralph Avalon <amoedo.raphael@gmail.com>
2018-06-05 08:38:33 -0300

    Add search posts

--- frontend/src/pages/components/sidebar/index.js
+++ frontend/src/pages/components/sidebar/index.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Sidebar extends Component {
+    
+    state = {
+        query: '',
+        postsFound: []
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query: query })
+
+        const { posts } = this.props;
+
+        if( !!query.trim() ) {
+            this.setState({ postsFound: posts.filter(post => post.title.toLowerCase().includes(query.trim().toLowerCase()) ) });
+        } else {
+            this.setState({ postsFound: [] });
+        }
+    }
+
     render() {
+        const { query, postsFound } = this.state;
+
         return (
             <div className="navbar-default sidebar" role="navigation">
                 <div className="sidebar-nav navbar-collapse">
                     <ul className="nav" id="side-menu">
                         <li className="sidebar-search">
                             <div className="input-group custom-search-form">
-                                <input type="text" className="form-control" placeholder="Search..." />
+                                <input type="text" className="form-control" placeholder="Search posts..." value={query} onChange={(event) => this.updateQuery(event.target.value)} />
                                 <span className="input-group-btn">
                                     <button className="btn btn-default" type="button">
                                         <i className="fa fa-search"></i>
@@ -17,11 +38,15 @@ class Sidebar extends Component {
                                 </span>
                             </div>
                         </li>
-                        <li>
-                            <Link to="/">
-                                <i className="fa fa-comment fa-fw"></i> Categories
-                            </Link>
-                        </li>
+                        {
+                            postsFound.map((post) => 
+                            <li key={post.id}>
+                                <Link to={`/${post.category}/${post.id}`}>
+                                    <i className="fa fa-comment fa-fw"></i> {post.title}
+                                </Link>
+                            </li>)
+                        }
+                        
                     </ul>
                 </div>
             </div>
@@ -29,4 +54,10 @@ class Sidebar extends Component {
     }
 }
 
-export default Sidebar;
+const mapStateToProps = ({ post }) => ({
+    posts: post.posts
+  });
+
+  export default connect(
+    mapStateToProps
+  )(Sidebar);
