b34e20d4010b82f0b512c36aac95e4868b5719e5
Ralph Avalon <amoedo.raphael@gmail.com>
2018-06-19 21:43:48 -0300

    Create 404 page

---
+++ frontend/src/pages/NotFound.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import HeaderText from '../components/headerText';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+class NotFound extends Component {
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <HeaderText title={`Post Not Found`} />
+        </div>
+
+        <div className="row">
+          <span>Sorry, we couldn't find your post. You can click <Link to={`/`}>here</Link> to see all posts.</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
