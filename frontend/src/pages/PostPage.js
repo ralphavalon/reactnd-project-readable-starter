09068f581101851c008e31ef6991f1168f0de4b1
Ralph Avalon <amoedo.raphael@gmail.com>
2018-06-19 21:52:47 -0300

    Add NotFound when post not found

--- frontend/src/pages/PostPage.js
+++ frontend/src/pages/PostPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import NotFound from './NotFound';
 import HeaderText from '../components/headerText';
 import CommentBox from '../components/commentBox';
 import PostDetailBox from '../components/postDetailBox';
@@ -8,47 +9,49 @@ import '../App.css';
 
 class PostPage extends Component {
 
-  componentDidMount() {
-    loadInitialData();
-  }
+    componentDidMount() {
+        loadInitialData();
+    }
 
-  render() {
-    const { post_id } = this.props.match.params;
-    const { posts, onUpvotePost, onDownvotePost } = this.props;
-    const post = posts.find(function (post) { return post.id === post_id; }) || {"title": ""};
-    
-    return (
-      <div>
-        <div className="row">
-          <HeaderText title={`Post: ${post.title}`} />
-        </div>
+    render() {
+        const { post_id } = this.props.match.params;
+        const { posts, onUpvotePost, onDownvotePost } = this.props;
+        const post = posts.find(function (post) { return post.id === post_id; });
 
-        <div className="row">
-          <PostDetailBox
-            post={post}
-            onUpvotePost={onUpvotePost}
-            onDownvotePost={onDownvotePost} 
-          />
-          <CommentBox post={post_id} />
-        </div>
-      </div>
-    );
-  }
+        if (!post) return <NotFound />;
+
+        return (
+            <div>
+                <div className="row">
+                    <HeaderText title={`Post: ${post.title}`} />
+                </div>
+
+                <div className="row">
+                    <PostDetailBox
+                        post={post}
+                        onUpvotePost={onUpvotePost}
+                        onDownvotePost={onDownvotePost}
+                    />
+                    <CommentBox post={post_id} />
+                </div>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = ({ category, post }) => ({
-  categories: category.categories,
-  posts: post.posts
+    categories: category.categories,
+    posts: post.posts
 });
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-      onUpvotePost: (data) => dispatch(upvotePost(data)),
-      onDownvotePost: (data) => dispatch(downvotePost(data))
-  }
+    return {
+        onUpvotePost: (data) => dispatch(upvotePost(data)),
+        onDownvotePost: (data) => dispatch(downvotePost(data))
+    }
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(PostPage);
